6983b9873fae7753ecff880895b618b6eec80196
Roberto1501 <robertocarvalho1501@yahoo.com>
2023-01-25 17:54:19 -0300

    Adicionei mudança de cor em base ao valor

--- tabela.js
+++ tabela.js
@@ -1,8 +1,18 @@
 function test(){
     let credito = Number(document.getElementById("row2-col3").value)
     let debito = Number(document.getElementById("row2-col4").value)
+    let creditoOutros = Number(document.getElementById("row2-col5").value)
+    let debitoOutros = Number(document.getElementById("row2-col6").value)
+    let FeirasDeb = Number(document.getElementById("row2-col7").value)
+    let estiloFinal = document.getElementById("row2-col8")
     let resultadoFinal = document.getElementById("row2-col8")
-    let resultado = credito-debito
+    let resultado = (credito+creditoOutros)-(debito+debitoOutros+FeirasDeb)
+    if(resultado<0){
+        estiloFinal.style.color = "red"
+    }
+    else{
+        estiloFinal.style.color = "black"
+    }
     resultadoFinal.value = resultado
      console.log(resultado)
 }
